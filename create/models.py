998e1b6a47465c82b3ea0522f00e6fe15d4548f6
SwapnaliJadhav <swapnalij91@gmail.com>
2023-07-04 06:41:09 +0100

    final

--- create/models.py
+++ create/models.py
@@ -6,6 +6,12 @@ class Sport(models.Model):
     slug = models.SlugField(max_length=50)
     active = models.BooleanField(default=False)
 
+    def update_activity_status(self):
+        if not self.event_set.filter(active=True).exists():
+            self.active = False
+        else:
+            self.active = True
+        self.save()
 
 class Event(models.Model):
     name = models.CharField(max_length=50)
@@ -17,11 +23,29 @@ class Event(models.Model):
     scheduled_start = models.DateTimeField()
     actual_start = models.DateTimeField(null=True, blank=True)
 
-   
+    def update_activity_status(self):
+        if not self.selection_set.filter(active=True,outcome='unsettled').exists():
+            self.active = False
+        else:
+            self.active = True
+        self.save()
+        self.sport.update_activity_status()
+    def save(self, *args, **kwargs):
+        if self.status == 'started' and not self.actual_start:
+            self.actual_start = timezone.now()
+        super().save(*args, **kwargs)
+
 class Selection(models.Model):
     name = models.CharField(max_length=50)
     event = models.ForeignKey(Event, on_delete=models.CASCADE)
     price = models.DecimalField(max_digits=8, decimal_places=2)
     active = models.BooleanField(default=False)
     outcome = models.CharField(max_length=10)
-
+    
+    def save(self, *args, **kwargs):
+        if self.outcome != 'unsettled':
+            self.active = False
+        else:
+            self.active = True
+        super().save(*args, **kwargs)
+        self.event.update_activity_status()
