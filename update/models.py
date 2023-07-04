4da6bdf495789f452fb338558068c3c33cef03d7
SwapnaliJadhav <swapnalij91@gmail.com>
2023-07-04 04:51:50 +0100

    final

--- update/models.py
+++ update/models.py
@@ -6,6 +6,15 @@ class Sport(models.Model):
     slug = models.SlugField(max_length=50)
     active = models.BooleanField(default=False)
 
+    def __str__(self):
+        return self.name
+    def update_active_status(self):
+        if not self.event_set.filter(active=True).exists():
+            self.active = False
+        else:
+            self.active = True
+        self.save()
+
 class Event(models.Model):
     name = models.CharField(max_length=50)
     slug = models.SlugField(max_length=50)
@@ -15,7 +24,16 @@ class Event(models.Model):
     status = models.CharField(max_length=10)
     scheduled_start = models.DateTimeField()
     actual_start = models.DateTimeField(null=True, blank=True)
+
     def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+        if not self.selection_set.filter(active=True).exists():
+            self.active = False
+        else:
+            self.active = True
+
+        self.sport.update_active_status()
+
         if self.status.lower() == "started" and not self.actual_start:
             self.actual_start = timezone.now()
         elif self.status.lower() != "started" and self.actual_start:
@@ -23,10 +41,24 @@ class Event(models.Model):
 
         super().save(*args, **kwargs)
 
+    def __str__(self):
+        return self.name
+
 class Selection(models.Model):
     name = models.CharField(max_length=50)
     event = models.ForeignKey(Event, on_delete=models.CASCADE)
     price = models.DecimalField(max_digits=8, decimal_places=2)
     active = models.BooleanField(default=False)
     outcome = models.CharField(max_length=10)
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+        if not self.event.selection_set.filter(active=True).exists():
+            self.event.active = False
+        else:
+            self.event.active = True
+
+        self.event.sport.update_active_status()
+
+    def __str__(self):
+        return self.name
 
